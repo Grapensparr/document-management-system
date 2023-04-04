9b4e0190d66f8c29a703db3151b3a1da3544ad6d
Grapensparr <liza.grapensparr@gmail.com>
2023-04-04 09:31:51 +0200

    Editor added

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,14 @@
 import createNewDocument from "./createNewDocument.js";
 
+tinymce.init ({
+    selector: '#documentContent',
+    toolbar: 'undo redo | fontfamily fontsize | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent',
+
+    setup: function(editor) {
+        editor.on('change', function() {
+            editor.save();
+        });
+    }
+});
+
 localStorage.setItem('loggedInUser', 'Liza');
